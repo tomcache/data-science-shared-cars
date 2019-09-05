356aa6347b5cfab646582cfebd13b377b47750dd
Tom Linnell <telemeter@protonmail.com>
2019-09-05 07:54:30 -0400

    latest updates for flow

--- shared.R
+++ shared.R
@@ -52,7 +52,7 @@ length(unique(sharedAutos$total_cars))
 
 db <- read_csv("autotel//sample_table.csv") %>% 
   mutate(timestamp = as.POSIXct(timestamp)) %>% 
-  mutate(timestamp = timestamp + 3600*2, # local time
+  mutate(timestamp = timestamp + 3600*2, # local time = GMT + 2
          Hour = lubridate::hour(timestamp),
          Minute = lubridate::minute(timestamp),
          Weekday = lubridate::wday(timestamp)
@@ -63,7 +63,7 @@ dbgrid <- db %>%
          Grid_long = round(longitude,4)
  )
 
-
+dbgrid_day1 <- dbgrid %>% filter(as_date(timestamp) == sample_day1 )
 
 # Take a look at the Timestamp and get it into the correct format
 class(sharedAutos$timestamp)
@@ -153,11 +153,11 @@ hist(carCounts_loc$n)
 # 0.001 unit of latitude and longitude will give us the size (~ 110 x 90M)
 # we are looking for:
 
-dbgrid %>% group_by(Grid_lat, Grid_long, Hour) %>% summarize(grid_total_cars = sum(total_cars)) %>%
+dbgrid_day1 %>% group_by(Grid_lat, Grid_long, Hour) %>% summarize(grid_total_cars = sum(total_cars)) %>%
   ggplot(aes(Grid_long, Grid_lat, color = grid_total_cars)) + geom_point() +
   facet_wrap(~Hour)
 
-cars_by_grid <- dbgrid %>% group_by(Grid_lat, Grid_long, Hour) %>% 
+cars_by_grid <- dbgrid_day1 %>% group_by(Grid_lat, Grid_long, Hour) %>% 
   summarize(grid_total_cars = sum(total_cars))
 
 (max(cars_by_grid$grid_total_cars))
@@ -172,10 +172,54 @@ hist(db$total_cars, col = "slategray4")
 
 hist(cars_by_grid$grid_total_cars, 
      xlim = c(0,100),
-     col = slategray4,
-     breaks = c(0,1,2,3,4,5,10,15,20,25,30,35,40,45,50,60,70,80,90,100,3000))
+     col = "slategray4",
+     breaks = c(0,1,2,3,4,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,3000))
 
 plot(density(cars_by_grid$grid_total_cars))
+
+# Calculate ho many cars leave and enter a grid by timestamp:
+dbcars <-  db %>% 
+  select(carsList) %>% 
+  distinct() %>% 
+  mutate(cars = sub(carsList, pattern = "\\[", replacement = "")) %>% 
+  mutate(cars = sub(cars, pattern = "\\]", replacement = "")) %>% 
+  separate(cars, sep = ",", into = c("car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9", "car10"), fill = "right") %>% 
+  mutate_at(2:11, as.numeric)
+# And, let's have a list of all cars "names" (which is a number)
+CarsList <- (1:max(dbcars$car1, na.rm = T))[1:max(dbcars$car1, na.rm = T) %in% dbcars$car1]
+# While there are 261 cars, some numbers are not in use (probably anymore), and we'll find to have the last car as # 272
+
+# Now we can create a dataframe where each row is only for a specifc car, whose number will be in a new column: "Car"
+# Also, we don't need to have the data on each car every 2 minutes, as most of the day it does not move
+# So we'll have a row only for each change in location of a car
+
+dbByCar <- left_join(db, dbcars) %>% 
+  gather(CarI, Car, car1:car10)  %>% 
+  filter(!is.na(Car)) %>% 
+  group_by(Car) %>% 
+  arrange(timestamp) %>% 
+  # let's disregard small changes in location which are probably due to GPS errors
+  mutate(Delta = abs(latitude - lag(latitude, default = 0)) + 
+           abs(longitude - lag(longitude, default = 0)) > 0.002) %>% 
+  mutate(
+    LastRideTime = ifelse(Delta == TRUE, as.numeric(difftime(timestamp, lag(timestamp), units = "mins")), 0) # assuming ride time = time car is not available in the db
+  ) %>% 
+  filter(Delta == TRUE) %>% # only take rows where the location changed
+  mutate(
+    StartTime = timestamp - 60*LastRideTime,
+    StartLng = lag(longitude), StartLat = lag(latitude),
+    Day = lubridate::day(StartTime)
+  ) %>% 
+  ungroup()
+
+
+dbByCarGrid <- dbByCar %>%
+  mutate(Grid_lat = round(latitude,4),
+         Grid_long = round(longitude,4)
+  )
+
+dbByCar %>% group_by(Grid_lat, Grid_long,)
+
 
 sample_summary <- sharedAutos_day %>% group_by(latitude, longitude) %>% summarize(n = n())
 
