56d9b09e2db65d99846c79f97854f725c6808b50
Tom Linnell <telemeter@protonmail.com>
2019-09-10 07:51:10 -0400

    Initial markdown

--- shared.R
+++ shared.R
@@ -21,11 +21,16 @@ if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(ggmap)) install.packages("ggmap", repos = "http://cran.us.r-project.org")
 if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 
 
 
 dl <- tempfile()
 download.file("https://www.kaggle.com/doit-intl/autotel-shared-car-locations/downloads/autotel-shared-car-locations.zip", dl)
+
+unzip("autotel-shared-car-locations.zip", exdir = "autotel")
+
 
 sharedAutos <- read.csv("autotel/sample_table.csv", stringsAsFactors = FALSE)
 
@@ -61,10 +66,157 @@ db <- read_csv("autotel//sample_table.csv") %>%
          Weekday = lubridate::wday(timestamp)
   )
 
+# We next add a grid identifier, using a grid size of approximately 10m square
+
 dbgrid <- db %>%
   mutate(Grid_lat = round(latitude,4),
-         Grid_long = round(longitude,4)
+         Grid_long = round(longitude,4),
+         gridKey = (Grid_lat*10000000000 + Grid_long*10000)
  )
+
+dbcars <-  db %>% 
+  select(carsList) %>% 
+  distinct() %>% 
+  mutate(cars = sub(carsList, pattern = "\\[", replacement = "")) %>% 
+  mutate(cars = sub(cars, pattern = "\\]", replacement = "")) %>% 
+  separate(cars, sep = ",", into = c("car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9", "car10"), fill = "right") %>% 
+  mutate_at(2:11, as.numeric)
+# And, let's have a list of all cars "names" (which is a number)
+CarsList <- (1:max(dbcars$car1, na.rm = T))[1:max(dbcars$car1, na.rm = T) %in% dbcars$car1]
+# While there are 261 cars, some numbers are not in use (probably anymore), and we'll find to have the last car as # 272
+
+# Now we can create a dataframe where each row is only for a specifc car, whose number will be in a new column: "Car"
+# Also, we don't need to have the data on each car every 2 minutes, as most of the day it does not move
+# So we'll have a row only for each change in location of a car
+
+# create a list of entries separated by car, and add grid information:
+
+# this grid is coarse, roughly 1km^2 in area:
+
+dbByCarGridL <- left_join(db, dbcars) %>% 
+  gather(CarI, Car, car1:car10)  %>% 
+  filter(!is.na(Car)) %>% 
+  group_by(Car) %>% 
+  arrange(timestamp) %>% 
+  mutate(Grid_lat = round(latitude,2),
+         Grid_long = round(longitude,2)
+  ) %>%
+  mutate(gridKey = (Grid_lat*10000000000 + Grid_long*10000)) %>%
+  ungroup()
+  
+  # this is a finer-grained grid, 10m x 9m
+  
+  dbByCarGridF <- left_join(db, dbcars) %>% 
+    gather(CarI, Car, car1:car10)  %>% 
+    filter(!is.na(Car)) %>% 
+    group_by(Car) %>% 
+    arrange(timestamp) %>% 
+    mutate(Grid_lat = round(latitude,4),
+           Grid_long = round(longitude,4)
+    ) %>%
+    mutate(gridKey = (Grid_lat*10000000000 + Grid_long*10000)) %>%
+  ungroup()  
+  
+# range of full dataset sample:
+min(db$timestamp)
+max(db$timestamp)
+  
+#------------------------------------------------------------------
+#
+# Demand analysis
+db %>% group_by(timestamp) %>% summarize(cars_available = sum(total_cars)) %>%
+    ggplot(aes(timestamp, cars_available)) + geom_line() +
+    geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95)
+  
+#------------------------------------------------------------------
+#
+# Create cars summary by grid:
+dbGridSum <- dbgrid %>% group_by(timestamp, gridKey) %>% summarize(carTotal = sum(total_cars)) 
+  
+# dbGridSum %>% ggplot(aes(gridKey, carTotal, group = timestamp)) + geom_line()  
+
+
+#------------------------------------------------------------------
+#
+# Create train & test sets, as well as single day set for rapid testing and development
+#
+# Validation set will be 10% of MovieLens data
+
+set.seed(1, sample.kind="Rounding")
+# if using R 3.5 or earlier, use `set.seed(1)` instead
+test_index <- createDataPartition(y = dbgrid$total_cars, times = 1, p = 0.1, list = FALSE)
+db_train <- dbgrid[-test_index,]
+db_test <- dbgrid[test_index,]
+
+# We will now create a single-day data set, and do the same thing:
+
+sample_day1 <- ymd("2018-12-15")
+
+dbg_day1 <- dbgrid %>% filter(as_date(timestamp) == sample_day1 )
+
+set.seed(1, sample.kind="Rounding")
+# if using R 3.5 or earlier, use `set.seed(1)` instead
+test_index <- createDataPartition(y = dbg_day1$total_cars, times = 1, p = 0.1, list = FALSE)
+dbg1_train <- dbg_day1[-test_index,]
+dbg1_test <- dbg_day1[test_index,]
+
+#------------------------------------------------------------------
+#
+# For our initial prediction model, we need to compute the average car availability
+# by grid:
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+
+dbTrainMean <- db_train %>% summarize(Mean = mean(total_cars)) 
+mu_dbTrain <- mean(db_train$total_cars)
+
+dbG1TrainMean <- dbg1_train %>% summarize(Mean = mean(total_cars)) 
+mu_dbg1Train <- mean(dbg1_train$total_cars)
+
+(baseline_rmse <- RMSE(db_test$total_cars, mu_dbTrain))
+
+(baseline_rmse1 <- RMSE(dbg1_test$total_cars, mu_dbTrain))
+
+
+# Just for giggles, lets try the whole enchilada:
+# First on our dev set:
+mod0 <- lm(total_cars ~ Hour + gridKey + Weekday, data = dbg1_train)
+
+predictions0 <- predict(mod0, dbg1_test)
+
+RMSE(dbg1_test$total_cars, predictions0)
+
+# Then the full set:
+mod1 <- lm(total_cars ~ Hour + gridKey + Weekday, data = db_train)
+
+predictions <- predict(mod1, db_test)
+
+RMSE(db_test$total_cars, predictions)
+
+# WE can now explore some additional features of the caret package:
+
+# first we need to convert our outcome to a factor:
+
+dbg1_train[["total_cars"]] = factor(dbg1_train[["total_cars"]])
+
+
+train_glm1 <- train(total_cars ~ Hour + gridKey + Weekday, method = "glm", data = dbg1_train)
+train_knn1 <- train(total_cars ~ Hour + gridKey + Weekday, method = "knn", data = dbg1_train)
+
+
+
+y_hat_glm <- predict(train_glm1, dbg1_test, type = "raw")
+y_hat_knn <- predict(train_knn1, dbg1_test, type = "raw")
+  
+confusionMatrix(y_hat_glm, dbg1_test$total_cars)$overall[["Accuracy"]]
+confusionMatrix(y_hat_knn, dbg1_test$total_cars)$overall[["Accuracy"]]
+
+(glm_RMSE <- RMSE(dbg1_test$total_cars, y_hat_glm))
+
+(knn_RMSE <- RMSE(dbg1_test$total_cars, y_hat_knn))
 
 #------------------------------------------------------------------
 # 
@@ -73,7 +225,7 @@ dbgrid <- db %>%
 #For our first sample, we choose December 15, 2018 (Tuesday):
 sample_day1 <- ymd("2018-12-15")
 
-dbgrid_day1 <- dbgrid %>% filter(as_date(timestamp) == sample_day1 )
+# dbgrid_day1 <- dbgrid %>% filter(as_date(timestamp) == sample_day1 )
 
 #For our second sample, we choose December 18, 2018 (Friday):
 sample_day2 <- ymd("2018-12-21")
@@ -82,8 +234,131 @@ sample_day2 <- ymd("2018-12-21")
 sample_day3 <- ymd("2019-01-09")
 
 
+dbByCarGridL_day1 <- dbByCarGridL %>% filter(as_date(timestamp) == sample_day1 )
+
+dbByCarGridL_day2 <- dbByCarGridL %>% filter(as_date(timestamp) == sample_day2 )
+
+dbByCarGridL_day3 <- dbByCarGridL %>% filter(as_date(timestamp) == sample_day3 )
+
+# we will also create 1 fine-grained map for comparison:
+
+dbByCarGridS_day3 <- dbByCarGridS %>% filter(as_date(timestamp) == sample_day2 )
+
+#------------------------------------------------------------------
+#
+# Flow analysis - to understand where the demand is, lets look at the movement of 
+# cars between grids
+
+# First we calculate how many cars are in each grid for each hour of the day:
+
+flowByGrid <- dbByCarGridL_day1 %>% group_by(Grid_lat, Grid_long, Hour) %>%
+  summarize(inventory = n_distinct(Car))
+
+flowByGrid2 <- dbByCarGridL_day2 %>% group_by(Grid_lat, Grid_long, Hour) %>%
+  summarize(inventory = n_distinct(Car))
+
+flowByGrid3 <- dbByCarGridL_day3 %>% group_by(Grid_lat, Grid_long, Hour) %>%
+  summarize(inventory = n_distinct(Car))
+
+flowByGrid4 <- dbByCarGridS_day3 %>% group_by(Grid_lat, Grid_long, Hour) %>%
+  summarize(inventory = n_distinct(Car))
+
+# lets take a look at where the cars are:
+flowByGrid %>% ggplot(aes(Grid_long, Grid_lat, color = inventory)) + 
+  geom_point(size = 3) +
+  scale_color_gradient2(midpoint = 1, low = "blue",  mid = "gold",
+                        high = "red", space = "Lab" ) +
+  facet_wrap(~Hour)
+
+# Calculate the change in inventory by grid per hour:
+
+flowByGrid <- 
+  flowByGrid %>%
+  group_by(Grid_lat, Grid_long) %>%
+  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
+
+flowByGrid2 <- 
+  flowByGrid2 %>%
+  group_by(Grid_lat, Grid_long) %>%
+  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
+
+flowByGrid3 <- 
+  flowByGrid3 %>%
+  group_by(Grid_lat, Grid_long) %>%
+  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
+
+flowByGrid4 <- 
+  flowByGrid4 %>%
+  group_by(Grid_lat, Grid_long) %>%
+  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
+# Plot the results:
+
+flowByGrid %>% 
+  filter(carFlow != 0) %>%
+  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Hourly flow Day 1 Large  Grid") +
+  theme_dark() + facet_wrap(~Hour)
+
+flowByGrid2 %>% 
+  filter(carFlow != 0) %>%
+  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Hourly flow Day 2 Large  Grid") +
+  theme_dark() + facet_wrap(~Hour)
+
+flowByGrid3 %>% 
+  filter(carFlow != 0) %>%
+  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Hourly flow Day 3 Large Grid") +
+  theme_dark() + facet_wrap(~Hour)
+
+flowByGrid4 %>% 
+  filter(carFlow != 0) %>%
+  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Hourly flow Day 3 Fine Grid") +
+  theme_dark() + facet_wrap(~Hour)
 
 
+# First, define the box for Tel Aviv:
+# tav_top <- 32.1500
+# tav_left <- 34.7000
+# tav_bot <- 32.0000
+# tav_right <- 34.8500
+
+tav_top <- 32.1500
+tav_left <- 34.7200
+tav_bot <- 32.0000
+tav_right <- 34.8500
+
+tav_borders <- c(left = tav_left,
+                 right = tav_right,
+                 top = tav_top,
+                 bottom = tav_bot)
+
+tav_map <- get_stamenmap(bbox = tav_borders, zoom = 11)
+
+p <- ggmap(tav_map)
+
+f4 <- flowByGrid4 %>% filter(carFlow != 0 & Hour == 15)
+
+f3 <- flowByGrid3 %>% filter(carFlow != 0 & Hour == 8)
+
+p + geom_point(aes(Grid_long, Grid_lat, color = carFlow), data = f4, size = 3) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "slategray4",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Tel Aviv car flow 1/9/19 3pm Fine Grid")
+
+p + geom_point(aes(Grid_long, Grid_lat, color = carFlow), data = f3, size = 3) +
+  scale_color_gradient2(midpoint = 0, low = "blue", mid = "slategray4",
+                        high = "red", space = "Lab" ) +
+  labs(title = "Tel Aviv car flow 1/9/19 8am Large Grid")
 
 # Take a look at the Timestamp and get it into the correct format
 class(sharedAutos$timestamp)
@@ -211,20 +486,6 @@ flow %>% ggplot(aes(Grid_long, Grid_lat, color = grid_total_cars)) + geom_point(
   facet_wrap(~Hour)
 
 # Calculate ho many cars leave and enter a grid by timestamp:
-dbcars <-  db %>% 
-  select(carsList) %>% 
-  distinct() %>% 
-  mutate(cars = sub(carsList, pattern = "\\[", replacement = "")) %>% 
-  mutate(cars = sub(cars, pattern = "\\]", replacement = "")) %>% 
-  separate(cars, sep = ",", into = c("car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9", "car10"), fill = "right") %>% 
-  mutate_at(2:11, as.numeric)
-# And, let's have a list of all cars "names" (which is a number)
-CarsList <- (1:max(dbcars$car1, na.rm = T))[1:max(dbcars$car1, na.rm = T) %in% dbcars$car1]
-# While there are 261 cars, some numbers are not in use (probably anymore), and we'll find to have the last car as # 272
-
-# Now we can create a dataframe where each row is only for a specifc car, whose number will be in a new column: "Car"
-# Also, we don't need to have the data on each car every 2 minutes, as most of the day it does not move
-# So we'll have a row only for each change in location of a car
 
 dbByCar <- left_join(db, dbcars) %>% 
   gather(CarI, Car, car1:car10)  %>% 
@@ -261,21 +522,8 @@ sample_summary <- sharedAutos_day %>% group_by(latitude, longitude) %>% summariz
 hist(sample_summary$n)
 
 
-dbByCarA <- left_join(db, dbcars) %>% 
-  gather(CarI, Car, car1:car10)  %>% 
-  filter(!is.na(Car)) %>% 
-  group_by(Car) %>% 
-  arrange(timestamp) %>% 
-  mutate(Grid_lat = round(latitude,2),
-         Grid_long = round(longitude,2)
-  ) %>%
-  ungroup()
 
-dbByCarA_day1 <- dbByCarA %>% filter(as_date(timestamp) == sample_day1 )
 
-dbByCarA_day2 <- dbByCarA %>% filter(as_date(timestamp) == sample_day2 )
-
-dbByCarA_day3 <- dbByCarA %>% filter(as_date(timestamp) == sample_day3 )
 
 
 flow_start <- dbByCarA_day1 %>% group_by(Grid_lat, Grid_long, Hour) %>% filter(Minute == 0) %>%
@@ -310,42 +558,13 @@ flowByGrid2 <- dbByCarA_day2 %>% group_by(Grid_lat, Grid_long, Hour) %>%
 flowByGrid3 <- dbByCarA_day3 %>% group_by(Grid_lat, Grid_long, Hour) %>%
   summarize(inventory = n_distinct(Car))
 
-# Calculate the change in inventory by grid per hour:
 
-flowByGrid <- 
-  flowByGrid %>%
-  group_by(Grid_lat, Grid_long) %>%
-  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
 
-flowByGrid2 <- 
-  flowByGrid2 %>%
-  group_by(Grid_lat, Grid_long) %>%
-  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
+# and conversely, where the cars aren't: (Nees more work, as flowByGrid has inventory > 1 always with
+# the current grid size)
 
-flowByGrid3 <- 
-  flowByGrid3 %>%
-  group_by(Grid_lat, Grid_long) %>%
-  mutate(carFlow = inventory - dplyr::lag(inventory, n = 1, default = NA, order_by = Hour))
 
-# Plot the results:
 
-flowByGrid %>% 
-  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
-  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
-                        high = "red", space = "Lab" ) +
-  theme_dark() + facet_wrap(~Hour)
-
-flowByGrid2 %>% 
-  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
-  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
-                        high = "red", space = "Lab" ) +
-  theme_dark() + facet_wrap(~Hour)
-
-flowByGrid3 %>% 
-  ggplot(aes(Grid_long, Grid_lat, color = carFlow)) + geom_point(size = 2) +
-  scale_color_gradient2(midpoint = 0, low = "blue", mid = "lightyellow",
-                        high = "red", space = "Lab" ) +
-  theme_dark() + facet_wrap(~Hour)
 #------------------------------------------------------------------
 # 
 # Prediction system
